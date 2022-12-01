4d709af79e27bfc3aaacafe9da49f8bf06ad70f4
AlexFly93 <muhabon93@gmail.com>
2022-12-01 15:35:05 +0200

    Add correct HW

--- index.js
+++ index.js
@@ -3,19 +3,19 @@
 
 
 function faktorialOfNum(num) {
-  if (typeof num !== "number") {
+  if (typeof num !== ("number" && "bigint")) {
     throw new TypeError("type not right");
-    }
-    if (num < 0) {
+  }
+    if (num < 0n) {
         throw new Error('num must be positive!')
     }
-  if (num === 0) {
-    return 1;
+  if (num === 0n) {
+    return 1n;
   }
-  return num * faktorialOfNum(num - 1);
+  return num * faktorialOfNum(num - 1n);
 }
 try {
-    console.log(faktorialOfNum(6));
+    console.log(faktorialOfNum(220n));
 } catch (error) {
     console.log(error)
 }
