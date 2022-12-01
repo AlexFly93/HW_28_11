cc7cc4593dbd782811f5939955741bae7b27cc5b
AlexFly93 <muhabon93@gmail.com>
2022-12-01 15:00:12 +0200

    Add completed homework

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 "use strict";
 
+
+
+function faktorialOfNum(num) {
+  if (typeof num !== "number") {
+    throw new TypeError("type not right");
+    }
+    if (num < 0) {
+        throw new Error('num must be positive!')
+    }
+  if (num === 0) {
+    return 1;
+  }
+  return num * faktorialOfNum(num - 1);
+}
+try {
+    console.log(faktorialOfNum(6));
+} catch (error) {
+    console.log(error)
+}
